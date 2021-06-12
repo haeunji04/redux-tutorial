e01cc671b9049456a4818a324e1e2e6722590dcc
haeunji04 <ppippu12@gmail.com>
2021-06-12 22:09:54 +0900

    210612 commit

--- src/components/CardComponents.jsx
+++ src/components/CardComponents.jsx
@@ -4,9 +4,10 @@ const Card = (props) => {
     return (
         <div className="card">
             <div className="card-body">
-                <h5 className="card-title">{props.user.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{props.user.company.name}</h6>
-                <p className="card-text">{props.user.company.catchPhrase}</p>
+                <h5 className="card-title">이름 : {props.user.name}</h5>
+                <span>email : {props.user.email}</span>
+                <h6 className="card-subtitle mb-2 text-muted">회사명 : {props.user.company.name}</h6>
+                <p className="card-text">캐치프래이즈 : {props.user.company.catchPhrase}</p>
             </div>
         </div>
     )
